bf970c5252b50e30bff59713fda2c86bfd7e3580
Your Name <your-email@example.com>
2026-04-06 22:39:06 +0700

    em day ss02

--- src/main/java/org/example/it210_session02/controller/AuthController.java
+++ src/main/java/org/example/it210_session02/controller/AuthController.java
@@ -1,5 +1,14 @@
 package org.example.it210_session02.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
 @WebServlet(urlPatterns = {"/login", "/logout"})
 public class AuthController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
